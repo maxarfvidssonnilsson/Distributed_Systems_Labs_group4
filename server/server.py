3853bf1c8d8acf6f8a7a0d219beb939c9cdc2384
Erik Magnusson <magnusserik@gmail.com>
2021-11-26 15:45:54 +0100

    Removed trace

--- server/server.py
+++ server/server.py
@@ -15,18 +15,6 @@ from threading import Thread
 from bottle import Bottle, run, request, template
 import requests
 
-def tracefunc(frame, event, arg, indent=[0]):
-      if event == "call":
-          indent[0] += 2
-          print("-" * indent[0] + "> call function", frame.f_code.co_name)
-      elif event == "return":
-          print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
-          indent[0] -= 2
-      return tracefunc
-
-import sys
-sys.setprofile(tracefunc)
-
 # ------------------------------------------------------------------------------------------------------
 try:
     app = Bottle()
