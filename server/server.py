8a5b4515993c8bcda030fa45fa707e4127e983b0
Erik Magnusson <magnusserik@gmail.com>
2021-12-08 15:17:41 +0100

    .

--- server/server.py
+++ server/server.py
@@ -140,7 +140,7 @@ try:
         # for i in range(0, len(sorted_board)):
         #     board_dict.append(i, sorted_board.message)
 
-        board_dict = {k: v.message for k, v in sorted(board.iteritems())}
+        board_dict = {v.element_id: v.message for v in sorted(board.values())}
 
         
         return template('server/boardcontents_template.tpl',board_title='Vessel {}'.format(my_id), board_dict=board_dict)
