26111d897b932613984979166ab61f4cff167ae9
Erik Magnusson <magnusserik@gmail.com>
2021-12-17 16:13:52 +0100

    Documentation

--- server/server.py
+++ server/server.py
@@ -17,19 +17,22 @@ from bottle import Bottle, run, request, template
 import requests
 # ------------------------------------------------------------------------------------------------------
 
-class Element:
-    def __init__(self, action, element_id, message, vector_clock, time_stamp):
+# Post class is added to keep track of all the data related to each post and to streamline caprisons between different posts. 
+class Post:
+    def __init__(self, action, post_id, message, vector_clock, time_stamp):
         self.action = action
-        self.element_id = element_id
+        self.post_id = post_id
         self.message = message
         self.vector_clock = vector_clock
         self.time_stamp = time_stamp
-        
+
+    # Comparison method that is automatically called when a comparison between two posts is made. e.g. post1 > post2
+    # This comparison is used to consistelty determine the order of messages, and modification to messages. 
     def __lt__(self, other):
         # First we compare based on vector clocks. 
-        if compare_vector_clocks(self.vector_clock, other.vector_clock): 
+        if self.compare_vector_clocks(self.vector_clock, other.vector_clock): 
             return False
-        elif compare_vector_clocks(other.vector_clock, self.vector_clock):
+        elif self.compare_vector_clocks(other.vector_clock, self.vector_clock):
             return True
         else: 
             # If the vector clocks are in conflict we instead compare based on timestamps.
@@ -44,6 +47,13 @@ class Element:
                     return False
                 else:
                     return True
+    
+    # Returns True if clock_one is bigger or equals to clock_two for all nodes. 
+    def compare_vector_clocks(clock_one, clock_two):
+        for key in clock_one:
+            if clock_one[key] < clock_two[key]:
+                return False
+        return True
 
 try:
     app = Bottle()
@@ -51,139 +61,123 @@ try:
     #board stores all message on the system 
     board = {}
 
-
-
     # ------------------------------------------------------------------------------------------------------
     # BOARD FUNCTIONS
-    # You will probably need to modify them
     # ------------------------------------------------------------------------------------------------------
     
-    #This functions will add an new element
-    def add_new_element_to_store(element):
+    # This function will add a new post
+    def add_new_post_to_store(post):
         global board, my_id
         success = False
         try:
-           if element.element_id not in board:
-                board[element.element_id] = copy.deepcopy(element)
+           if post.post_id not in board:
+                # We do a deep copy so that edits in board wont change board_history. 
+                board[post.post_id] = copy.deepcopy(post)
                 success = True
         except Exception as e:
             print e
         return success
 
-    def modify_element_in_store(element):
+    # This function will modify a post
+    def modify_post_in_store(post):
         global board, my_id
         success = False
         try:
-            if element.element_id in board:
-                board[element.element_id].message = element.message
+            if post.post_id in board:
+                # Only the message is changed so that we still have the original timestamp and v-clock. 
+                board[post.post_id].message = post.message
                 success = True
         except Exception as e:
             print e
         return success
 
-    def delete_element_from_store(element):
+    # This function will modify a post 
+    def delete_post_from_store(post):
         global board, my_id
         success = False
-        popped = board.pop(element.element_id, False)
+        popped = board.pop(post.post_id, False)
         if popped:
             success = True
         else:
             print("Value to delete not found")
         return success
 
-    #------------------------------------------------
-    # HELPER METHODS:
-    #------------------------------------------------
-
-    # def sort_board():
-    #     global board
-    #     sorted_board = {}
-    #     for i in range(0, len(board)):
-    #         smallest_element = None
-    #         biggest_key = None
-
-    #         for key, element in board.items():
-    #             old_smallest_element = (smallest_element[3],smallest_element[4],smallest_element[5])
-    #             old_element = (element[3],element[4],element[5])
-
-    #             if smallest_element == determine_newest(old_smallest_element,old_element):
-    #                 smallest_element = element
-    #                 biggest_key = key
-
-    #         if smallest_element == None:
-    #             break
-    #         sorted_board[biggest_key] = smallest_element
-    #     board = sorted_board
-
-
- 
-
     # ------------------------------------------------------------------------------------------------------
     # ROUTES
     # ------------------------------------------------------------------------------------------------------
-    # a single example (index) for get, and one for post
-    # ------------------------------------------------------------------------------------------------------
-    #No need to modify this
     @app.route('/')
     def index():
         global board, my_id
-        board_iter = [(v.element_id, v.message) for v in sorted(board.values())]
+        if len(board) == 0:
+            board_iter = []
+        else:
+            board_iter = [(v.post_id, v.message) for v in sorted(board.values())]
         return template('server/index.tpl', board_title='Vessel {}'.format(my_id),
                 board_dict=board_iter, members_name_string='Erik Magnusson, Max Arfvidsson Nilsson')
 
     @app.get('/board')
     def get_board():
         global board, my_id
-        board_iter = [(v.element_id, v.message) for v in sorted(board.values())]
+        if len(board) == 0:
+            board_iter = []
+        else:
+            board_iter = [(v.post_id, v.message) for v in sorted(board.values())]
         return template('server/boardcontents_template.tpl',board_title='Vessel {}'.format(my_id), board_dict=board_iter)
-    
-    #------------------------------------------------------------------------------------------------------
-    
+     
     @app.post('/board')
     def client_add_received():
-        '''Adds a new element to the board
+        '''Adds a new post to the board
         Called directly when a user is doing a POST request on /board'''
         global board, my_id, vector_clock, board_history
         try:
             message = request.forms.get('entry')
             time_stamp = time.time()
-            element_id = hash(message + str(time_stamp))
-            vector_clock[str(my_id)] += 1
-            new_element = Element('ADD', element_id, message, copy.deepcopy(vector_clock), time_stamp)
-            add_new_element_to_store(new_element)
-            board_history.append(new_element)
+            
+            # Timestamp is added to the hash so that messages with the same text don't get the same hash.
+            # my_id is also added so that in the rare case that two nodes post the same message at the same 
+            # time they don't become the same message. 
+            post_id = hash(message + str(time_stamp) + str(my_id))  
 
-            threaded_propagate_to_vessels('/propagate/ADD/' + str(element_id), {'entry': new_element.message, 
-                'vector_clock': json.dumps(new_element.vector_clock), 'time_stamp': new_element.time_stamp})
+            vector_clock[str(my_id)] += 1
+            new_post = Post('ADD', post_id, message, copy.deepcopy(vector_clock), time_stamp)
+
+            # When adding things localy we can be sure that it won't conflict with posts already on the board. 
+            add_new_post_to_store(new_post)
+            board_history.append(new_post)
+
+            threaded_propagate_to_vessels('/propagate/ADD/' + str(post_id), {'entry': new_post.message, 
+                'vector_clock': json.dumps(new_post.vector_clock), 'time_stamp': new_post.time_stamp})
             return True
         except Exception as e:
             print e
         return False
 
-    @app.post('/board/<element_id:int>/')
-    def client_action_received(element_id):
+    @app.post('/board/<post_id:int>/')
+    def client_action_received(post_id):
         global board, my_id
         
-        print "You receive an element"
-        print "id is ", my_id
+        print("You receive a post")
+        print("id is ", my_id)
+        
         # Get the entry from the HTTP body
         message = request.forms.get('entry')
         delete_option = 'DELETE' if request.forms.get('delete') == '1' else 'MODIFY'
-        print("Delete option: " + delete_option)
         time_stamp = time.time()
         vector_clock[str(my_id)] += 1
-        new_element = Element(delete_option, element_id, message, copy.deepcopy(vector_clock), time_stamp)
+        new_post = Post(delete_option, post_id, message, copy.deepcopy(vector_clock), time_stamp)
 
-        board_history.append(new_element)
+        board_history.append(new_post)
+        # When deleting or modifying things localy we can be sure that the action won't have a conflict
+        # with what is already in the board. 
         if delete_option == 'DELETE': 
-            delete_element_from_store(new_element)
+            delete_post_from_store(new_post)
         else:
-            modify_element_in_store(new_element)
-        threaded_propagate_to_vessels('/propagate/' + delete_option + '/' + str(element_id), {'entry': new_element.message, 
-                'vector_clock': json.dumps(new_element.vector_clock), 'time_stamp': new_element.time_stamp})
+            modify_post_in_store(new_post)
         
-        print "the delete option is ", delete_option
+        threaded_propagate_to_vessels('/propagate/' + delete_option + '/' + str(post_id), {'entry': new_post.message, 
+                'vector_clock': json.dumps(new_post.vector_clock), 'time_stamp': new_post.time_stamp})
         
+    # Update foriegn parts of own vector clock to match incoming vector clock 
     def update_vector_clock(new_vector_clock):
         global vector_clock, my_id
         for key in vector_clock:
@@ -192,8 +186,8 @@ try:
         
 
     #With this function you handle requests from other nodes like add modify or delete
-    @app.post('/propagate/<action>/<element_id>')
-    def propagation_received(action, element_id):
+    @app.post('/propagate/<action>/<post_id>')
+    def propagation_received(action, post_id):
         #get entry from http body
         message = request.forms.get('entry')
         print("Incoming vector clock" + (request.forms.get('vector_clock')))
@@ -201,72 +195,55 @@ try:
         time_stamp = float(request.forms.get('time_stamp'))
         print "the action is", action
         update_vector_clock(input_vector_clock)
-        new_input = Element(action, int(element_id), message, input_vector_clock, time_stamp)
-        resolve_action(new_input)
-
-
-    def apply_action(element):
-        action = element.action
-        if action == "ADD":
-            add_new_element_to_store(element)
-        elif action =="MODIFY":
-            modify_element_in_store(element)
-        elif action == "DELETE":
-            delete_element_from_store(element)
-        else:
-           print("Action not valid")
-
-    def resolve_action(new_input):
+        new_post = Post(action, int(post_id), message, input_vector_clock, time_stamp)
+        resolve_action(new_post)
+    
+   
+   # This method checks to see that a new post doesn't conflict with the current board history. 
+   # If there is a conflict with the last post in board history reslove board is called.
+    def resolve_action(new_post):
         global board_history
-        if len(board_history) == 0 or new_input > board_history[-1]: # (new_input == determine_newest(new_input, board_history[-1])):
-            board_history.append(new_input)
-            apply_action(new_input)
+        # if the board is empty there is no conflict. 
+        # new_post > board_history can be done becuase of the built in comparison method in the Post class. 
+        if len(board_history) == 0 or new_post > board_history[-1]: 
+            board_history.append(new_post)
+            apply_action(new_post)
         else:
-            resolve_board(new_input)
+            resolve_board(new_post)
 
-    def resolve_board(new_input):
+    # This method slots in a new post in the right place in board_history
+    def resolve_board(new_post):
         global board, board_history
         sorted_history = []
         board_resolved = False
+        # boar_history is looped through until a place where new_post first is found
+        # new_post is placed in the correct slot and board_history is updated. 
         for historic_entry in board_history:
-            if board_resolved == True or new_input > historic_entry: #  new_input == determine_newest(historic_entry, new_input):
+            # new_post > historic_entry can be done becuase of the built in comparison method in the Post class. 
+            if board_resolved == True or new_post > historic_entry: 
                 sorted_history.append(historic_entry)
             else:
-                sorted_history.append(new_input)
+                sorted_history.append(new_post)
                 sorted_history.append(historic_entry)
                 board_resolved = True
         board_history = sorted_history
 
+        # board is updated by re-applying the entire board_history on an empty board
         board = []
         for historic_entry in board_history:
             apply_action(historic_entry)
 
-    # def determine_newest(element_1, element_2):
-    #     # First we compare based on vector clocks. 
-    #     if compare_vector_clocks(element_1.vector_clock, element_2.vector_clock): 
-    #         return element_1
-    #     elif compare_vector_clocks(element_2.vector_clock, element_1.vector_clock):
-    #         return element_2
-    #     else: 
-    #         # If the vector clocks are in conflict we instead compare based on timestamps.
-    #         if element_1.time_stamp > element_2.time_stamp:
-    #             return element_1
-    #         elif element_2.time_stamp > element_1.time_stamp:
-    #             return element_2
-    #         else:
-    #             # If the timestamps are equal we resolve what message to pick based on alphabetical order. 
-    #             # This makes sure we always have to same outcome in every node. 
-    #             if element_1.message < element_2.message:
-    #                 return element_1
-    #             else:
-    #                 return element_2
-                    
-    # Returns True if clock_one is bigger or equals to clock_two for all nodes. 
-    def compare_vector_clocks(clock_one, clock_two):
-        for key in clock_one:
-            if clock_one[key] < clock_two[key]:
-                return False
-        return True
+    # This method simply applies an action based on a post. 
+    def apply_action(post):
+        action = post.action
+        if action == "ADD":
+            add_new_post_to_store(post)
+        elif action =="MODIFY":
+            modify_post_in_store(post)
+        elif action == "DELETE":
+            delete_post_from_store(post)
+        else:
+           print("Action not valid")
 
     # ------------------------------------------------------------------------------------------------------
     # DISTRIBUTED COMMUNICATIONS FUNCTIONS
